e06341ec03c25d2406d2d5c0c16b33f2acbd2743
zIthalo <ithalowillian@gmail.com>
2022-10-20 17:09:14 -0300

    Create TesteTipos.java

---
+++ tiposprimitivosjava/TesteTipos.java
@@ -0,0 +1,10 @@
+package tiposprimitivosjava;
+
+import java.util.Scanner;
+
+public class TesteTipos {
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+        
+    }
+}
