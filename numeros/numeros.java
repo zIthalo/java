1d9b1000ec1eaa7a5d5fbbb9ba8e0b10585777f0
zIthalo <ithalowillian@gmail.com>
2022-10-20 11:26:38 -0300

    Update numeros.java

--- numeros/numeros.java
+++ numeros/numeros.java
@@ -6,7 +6,9 @@ public class numeros {
     public static void main(String[] args) {
         int n, s =0;
         String resp;
-        Scanner leia = new Scanner(System.in); /*new Scanner = objeto. Para criar uma classe Scanner você precisa colocar Scanner + o nome do scanner que você desejar, depois =, depois chamar o objeto new Scanner, e entre parêntesis colocar a classe de entrada(System.in);. Se fosse de saída seria System.out... ;) */ 
+        Scanner leia = new Scanner(System.in); /*new Scanner = objeto. 
+
+            Para criar uma classe Scanner você precisa colocar Scanner + o nome do scanner que você desejar, depois =, depois chamar o objeto new Scanner, e entre parêntesis colocar a classe de entrada(System.in);. Se fosse de saída seria System.out... ;) */ 
         do{
             System.out.println("Digite um número: ");
 
@@ -16,7 +18,7 @@ public class numeros {
 
             System.out.println("Quer continuar? [s/n]");
 
-            resp = leia.next(); /*aqui somente para strings */
+            resp = leia.next(); /*aqui somente para strings. ex. a variável resp recebe nome do scanner (leia) para string (.next()) */
 
         }while (resp.equals("s")); /*Lê-se: enquanto a var resp for igual a s. Nunca esquecer de colocar um ponto e vírgula aqui */
         System.out.println("A soma de todos os valores é igual a " + s);
